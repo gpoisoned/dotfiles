f3cbbe4e424504c2d7e47f67c2df47d0e7571435
Josh Warfield <josh@joinhandshake.com>
2015-10-02 17:37:35 -0600

    show hidden files fo realz

--- osx-setup.sh
+++ osx-setup.sh
@@ -16,6 +16,9 @@ sh -c "$(curl --remote-name https://raw.githubusercontent.com/thoughtbot/laptop/
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
 
+# Show user's Library folder
+chflags nohidden ~/Library
+
 # Install diff/merge tools
 brew install homebrew/x11/meld
 brew install kdiff3
