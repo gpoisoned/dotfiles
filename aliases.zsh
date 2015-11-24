30d6c1e6042f0a4078b59cafa3fd233cf7724bc3
Josh Warfield <josh@joinhandshake.com>
2015-11-24 10:32:19 -0700

    updates to prefs

--- aliases.zsh
+++ aliases.zsh
@@ -3,6 +3,6 @@ alias diffh="git difftool -d origin/stable...HEAD &"
 alias diffw="git difftool -d &"
 
 # This one depends on pick, which is not installed by general-setup.sh
-alias co="if [ ! -z `git status -s` ]; then echo 'commit or stash local changes first!'; else git checkout master && git pull && git branch -a | sed -e 's/remotes\/origin\///'| pick | xargs git checkout; fi"
+alias co="if [ ! -z `git status -s` ]; then echo 'commit or stash local changes first!'; else git remote update --prune && git branch -a | sed -e 's/remotes\/origin\///'| pick | xargs git checkout; fi"
 
 alias ss="rake dev:kill_db_connections && foreman start"
