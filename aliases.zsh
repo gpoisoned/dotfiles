17c77c02cbe69a96daf0eaaa052795e351222f3a
Josh Warfield <josh@joinhandshake.com>
2016-04-18 14:50:50 -0600

    updates to prefs

--- aliases.zsh
+++ aliases.zsh
@@ -3,6 +3,6 @@ alias diffh="git difftool -d origin/stable...HEAD &"
 alias diffw="git difftool -d &"
 
 # This one depends on pick, which is not installed by general-setup.sh
-alias co="git remote update --prune && git branch --no-merged master | sed -e 's/remotes\/origin\///'| pick | xargs git checkout"
+alias co="git branch -a --no-merged master | sed -e 's/remotes\/origin\///' | sort | uniq | pick | xargs git checkout"
 
 alias ss="rake dev:kill_db_connections && foreman start"
